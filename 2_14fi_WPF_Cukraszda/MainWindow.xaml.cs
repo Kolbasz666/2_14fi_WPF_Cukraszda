32a7656a4cd684750676676abb11a17c969cf20a
zsar.daniel <zsar.daniel@C19-TANAR>
2025-04-03 18:41:15 +0200

    Add project files.

---
+++ 2_14fi_WPF_Cukraszda/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2_14fi_WPF_Cukraszda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void Buyer(object s, EventArgs e)
+        {
+            BuyerWindow b = new BuyerWindow() { Top = this.Top, Left = this.Left, Visibility = Visibility.Visible };
+            this.Close();
+        }
+        void Seller(object s, EventArgs e)
+        {
+            SellerWindow b = new SellerWindow() { Top = this.Top, Left = this.Left, Visibility = Visibility.Visible };
+            this.Close();
+        }
+    }
+}
