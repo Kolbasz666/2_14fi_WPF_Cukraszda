2464da00e7f1d8de66179919976e06f2f74ec51a
zsar.daniel <zsar.daniel@C19-TANAR>
2025-04-03 20:54:08 +0200

    End of first day

--- 2_14fi_WPF_Cukraszda/BuyerWindow.xaml.cs
+++ 2_14fi_WPF_Cukraszda/BuyerWindow.xaml.cs
@@ -19,9 +19,63 @@ namespace _2_14fi_WPF_Cukraszda
     /// </summary>
     public partial class BuyerWindow : Window
     {
+        List<Cake> allCake = new List<Cake>();
         public BuyerWindow()
         {
             InitializeComponent();
+            Start();
+        }
+        void Start()
+        {
+            allCake.Add(new Cake("Diós krémes", 1000, "https://www.szakacsreceptek.hu/_site_media/com_strava/mod_recepty/img_list-by-category/32.jpg") { id = "Kolbász" });
+            BuildItems();
+        }
+        void BuildItems()
+        {
+            foreach (Cake item in allCake)
+            {
+                Grid oneCake = new Grid();
+                oneCake.Height = 180;
+                oneCake.Width = 120;
+                for (int i = 0; i < 4; i++)
+                {
+                    RowDefinition row = new RowDefinition();
+                    if (i == 1)
+                        row.Height = new GridLength(2, GridUnitType.Star);
+                    oneCake.RowDefinitions.Add(row);
+                }
+                Label title = new Label() { Content = item.name };
+                oneCake.Children.Add(title);
+                Image picture = new Image();
+                picture.Source = new BitmapImage(new Uri(item.picture));
+                picture.Stretch = Stretch.UniformToFill;
+                oneCake.Children.Add(picture);
+                oneCake.Background = new SolidColorBrush(Colors.Bisque);
+                Grid.SetRow(picture, 1);
+                Label price = new Label() { Content = item.price + " Ft" };
+                oneCake.Children.Add(price);
+                Grid.SetRow(price, 2);
+                Border BuyBorder = new Border() { CornerRadius = new CornerRadius(10), Background = new SolidColorBrush(Colors.GreenYellow), Margin = new Thickness(10, 2, 10, 2) };
+                oneCake.Children.Add(BuyBorder);
+                Grid.SetRow(BuyBorder, 3);
+                Label BuyLabel = new Label() { Content = "Kosárba" };
+                BuyLabel.Tag = item.id;
+                BuyLabel.MouseLeftButtonUp += Buy;
+                BuyBorder.Child = BuyLabel;
+
+                Items.Children.Add(oneCake);
+            }
+        }
+        void Buy(object sender, EventArgs e)
+        {
+            if (!(sender is Label)) {
+                return;
+            }
+            Label oneLabel = sender as Label;
+            string id = oneLabel.Tag.ToString();
+            //Cake oneCake = allCake.Where(cake => cake.id == id).First();
+            //ugyanaz mint alatta
+            MessageBox.Show("Az elem ára: " + allCake.Where(cake => cake.id == id).First().price);
         }
     }
 }
---
+++ 2_14fi_WPF_Cukraszda/Cake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_14fi_WPF_Cukraszda
+{
+    public class Cake
+    {
+        public Cake() { }
+        public Cake(string name, int price, string picture)
+        {
+            this.name = name;
+            this.price = price;
+            this.picture = picture;
+        }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string picture { get; set; }
+        public string description { get; set; }
+        public string allergenes { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+        public string message { get; set; }
+    }
+}
